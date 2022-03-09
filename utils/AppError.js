194119ce5d17394224824d97c9f5d67a66daf8b3
Victor A. Alvarado <isc.cuvillas@gmail.com>
2022-03-08 21:51:07 -0600

    add constructor

--- utils/AppError.js
+++ utils/AppError.js
@@ -5,7 +5,7 @@ class AppError extends Error {
     this.message = message;
     this.status =
       Number(statuscode.toString().substring(0, 1)) <= 4 ? "error" : "fail";
-    Error.captureStackTrace(this);
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
